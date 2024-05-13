7f4e7663cae1bf525658a8010b5ae9d7b3f8c426
Álvaro Rosado <alvaro.rosado.g@gmail.com>
2024-05-13 21:59:19 +0200

    Adapted to new Cloudflare API

--- config.sh
+++ config.sh
@@ -1,11 +1,9 @@
-CLOUDFLARE_KEY=<input-your-apikey-here>
-CLOUDFLARE_EMAIL=<input-your-email-here>
+CLOUDFLARE_KEY="change-me"
 
 CHALLENGE_PREFIX="_acme-challenge"
 CHALLENGE_DOMAIN="${CHALLENGE_PREFIX}.${CERTBOT_DOMAIN}"
 PARENT_DOMAIN=$(sed 's/.*\.\(.*\..*\)/\1/' <<< "${CERTBOT_DOMAIN}")
 
 CLOUDFLARE_ZONE=$(curl -X GET "https://api.cloudflare.com/client/v4/zones?name=${PARENT_DOMAIN}" \
-     -H "X-Auth-Email: ${CLOUDFLARE_EMAIL}" \
-     -H "X-Auth-Key: ${CLOUDFLARE_KEY}" \
+     -H "Authorization: Bearer ${CLOUDFLARE_KEY}" \
      -H "Content-Type: application/json" -s | jq -r '.result[0].id')
